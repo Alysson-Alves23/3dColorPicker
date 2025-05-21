00258146aa8a652cf62c4e2c76e58dc093d08a93
Alysson de Jesus Alves <65509921+Alysson-Alves23@users.noreply.github.com>
2025-05-21 14:08:27 -0300

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  url: process.env.NEXT_PUBLIC_API_URL || "localhost",
-  port: process.env.NEXT_PUBLIC_API_PORT || "3000",
+  url: process.env.API_URL || "localhost",
+  port: process.env.API_PORT || "3000",
 };
 
 export default config;
